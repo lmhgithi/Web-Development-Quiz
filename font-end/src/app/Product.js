416c35d483961b72bed58ecc8ad3b60e9911b136
lmhgithi <lmhfox@foxmail.com>
2020-08-14 15:21:32 +0800

    [lmh] add add product button to font-end

--- font-end/src/app/Product.js
+++ font-end/src/app/Product.js
@@ -2,11 +2,31 @@ import React, { Component } from 'react';
 import img from '../images/cola.jpg'
 class Product extends Component {
     state = {
-        data: '',
+        handleFlag: false,
     }
-
+    handleClickaddProductButton = () => {
+        this.setState({
+            handleFlag: true,
+        })
+        URL = "http://localhost:8080/order"
+        fetch(URL, {
+            method:"POST",
+        }).then(Response => {
+            if (Response.status === 200) {
+                this.setState({
+                    handleFlag: false,
+                })
+                return Response.json();
+            }
+            else {
+                this.setState({
+                    handleFlag: false,
+                })
+                Promise.reject();
+            }
+        });
+    }
     render() {
-        console.log(this.state.data)
         return (
             <div className="product">
                 <img className='productImg' src={img} alt='placeHoder' />
@@ -14,6 +34,12 @@ class Product extends Component {
                     <h2>{this.props.product.name}</h2>
                     <p className='unit'>单价：{this.props.product.price}/{this.props.product.unit}</p>
                 </div>
+                <button className='addProductButton'
+                    onClick={this.handleClickaddProductButton}
+                    disabled={this.state.handleFlag}
+                >
+                    +
+                </button>
 
 
             </div>
